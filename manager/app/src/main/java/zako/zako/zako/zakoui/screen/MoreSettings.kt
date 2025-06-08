a06d5f0e7e55187c52c801cad8031a2b4383acb4
sti-233 <86592209+sti-233@users.noreply.github.com>
2025-06-09 05:21:28 +0800

    test: blur

--- manager/app/src/main/java/zako/zako/zako/zakoui/screen/MoreSettings.kt
+++ manager/app/src/main/java/zako/zako/zako/zakoui/screen/MoreSettings.kt
@@ -5,8 +5,6 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import android.graphics.RenderEffect
-import android.graphics.Shader
 import android.net.Uri
 import android.os.Build
 import android.widget.Toast
@@ -86,6 +84,8 @@ import androidx.compose.material3.Switch
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.RenderEffect
+import androidx.compose.ui.graphics.TileMode
 import com.sukisu.ultra.ui.theme.getCardColors
 import com.sukisu.ultra.ui.theme.getCardElevation
 import androidx.compose.material3.RadioButton
@@ -635,7 +635,7 @@ fun MoreSettingsScreen(
                         alpha = 0.99f
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                             renderEffect = RenderEffect.createBlurEffect(
-                                CardConfig.cardBlur, CardConfig.cardBlur, Shader.TileMode.MIRROR
+                                CardConfig.cardBlur, CardConfig.cardBlur, TileMode.Mirror
                             )
                         }
                     }
